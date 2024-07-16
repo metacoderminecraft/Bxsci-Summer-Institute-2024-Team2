8dd002e25407ebf69609d3ca413d42cbe3efd76a
kishan243 <116034882+kishan243@users.noreply.github.com>
2024-07-16 12:53:51 -0400

    fixed issue

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -1,6 +1,8 @@
-package frc.robot;
+package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drive extends SubsystemBase{
     
