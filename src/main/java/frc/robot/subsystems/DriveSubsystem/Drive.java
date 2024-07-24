242380e75fdb2bd7fb8d87458509ac87b7886ae2
cheny7@bxscience.edu <cheny7@bxscience.edu>
2024-07-24 10:18:20 -0400

    g

--- src/main/java/frc/robot/subsystems/DriveSubsystem/Drive.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem/Drive.java
@@ -6,14 +6,18 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drive extends SubsystemBase{  
-   
+  
     private final CANSparkMax leftLeader = new CANSparkMax(DriveConstants.kleftMotor1Port, MotorType.kBrushless);
     private final CANSparkMax leftfollower = new CANSparkMax(DriveConstants.kleftMotor2Port, MotorType.kBrushless);
     
     private final CANSparkMax rightLeader = new CANSparkMax(DriveConstants.krightMotor1Port, MotorType.kBrushless);
     private final CANSparkMax rightfollower = new CANSparkMax(DriveConstants.krightMotor2Port, MotorType.kBrushless);
-
+     
     public Drive() {
+        leftLeader.restoreFactoryDefaults();
+        leftfollower.restoreFactoryDefaults();
+        rightLeader.restoreFactoryDefaults();
+        rightfollower.restoreFactoryDefaults();
         rightLeader.setInverted(true);
         rightfollower.follow(rightLeader);
         leftfollower.follow(leftLeader);
